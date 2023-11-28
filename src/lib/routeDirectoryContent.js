2f76cac11f970edda6ad0da10248b5faae1cda5b
brianbancroft <hello@brianbancroft.ca>
2023-11-27 23:48:52 -0800

    Add comment

--- src/lib/routeDirectoryContent.js
+++ src/lib/routeDirectoryContent.js
@@ -1,3 +1,5 @@
+// This element is separate as the glob syntax breaks syntax highlighting in vscode
+
 const ROUTES = import.meta.globEager(`/src/routes/*.jsx`);
 
 export const routeDirectoryContent = Object.entries(ROUTES).map(
